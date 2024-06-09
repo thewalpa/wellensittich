ada1a4413f3b91bf3ee1b47eeb5df0c5e654a34b
thewalpa <paul@thewalt.de>
2024-06-09 12:32:43 +0200

    youtube search tuning

--- interfaces/musicsearch/youtube.go
+++ interfaces/musicsearch/youtube.go
@@ -27,7 +27,7 @@ func NewYoutubeMusicSearch(api_key string) *YoutubeMusicSearch {
 }
 
 func (yts *YoutubeMusicSearch) SearchPlay(query string) (MusicSearchResult, error) {
-	url := "https://youtube.googleapis.com/youtube/v3/search" + fmt.Sprintf("?type=video&q=%v&key=%v&limit=1", url.QueryEscape(query), yts.api_key)
+	url := "https://youtube.googleapis.com/youtube/v3/search" + fmt.Sprintf("?part=snippet&videoCategoryId=10&type=video&q=%v&key=%v", url.QueryEscape(query), yts.api_key)
 	response, err := http.Get(url)
 	if err != nil {
 		return MusicSearchResult{}, err
